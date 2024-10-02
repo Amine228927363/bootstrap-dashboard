50d76bc9d9869c223c822dc1cc9beccad790e950
Mohamed-Amine SALAH (EXT) <mohamed-amine.salah.ext@valeo.com>
2024-10-02 22:12:13 +0100

    Your commit message here

--- bootstrap-4.0.0-dist/assets/js/script.js
+++ bootstrap-4.0.0-dist/assets/js/script.js
@@ -19,7 +19,23 @@
     // Append sorted rows back to the table body
     rows.forEach((row) => tableBody.appendChild(row));
   }
-
+//form validation 
+(function () {
+  'use strict'
+  const forms = document.querySelectorAll('.requires-validation')
+  Array.from(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+  
  
   
  
